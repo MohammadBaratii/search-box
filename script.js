9f9a4466b9ba3769514690dad714139fc816bbf4
Mohammad Barati <m.mohammadbarati.b@gmail.com>
2021-05-08 11:13:46 +0430

    add all files

---
+++ script.js
@@ -0,0 +1,18 @@
+const $ = document;
+
+//select
+let container = $.querySelector(".container");
+let inp = $.querySelector("input");
+let section = $.querySelector("section");
+
+//events
+container.addEventListener("mouseover", () => {
+  inp.style.width = "20rem";
+  inp.style.marginRight = "2rem";
+  inp.style.padding = "1rem 0 1rem 1rem";
+});
+section.addEventListener("click", () => {
+  inp.style.width = "0rem";
+  inp.style.marginRight = "0rem";
+  inp.style.padding = "0";
+});
